11528f26261943fa7ee2aa23457e92a2af13c688
Sherina <137211921+sheri-na@users.noreply.github.com>
2024-04-06 23:42:30 +0000

    tyring to do the parralax and an animation on the hero section

---
+++ script.js
@@ -0,0 +1,5 @@
+const dropDownMenu = document.querySelector(".dropdown-menu")
+const togglemenu = document.querySelector(".bx-menu")
+togglemenu.onclick = function(){
+  dropDownMenu.classList.toggle("open")
+}
