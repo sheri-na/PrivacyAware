f9f9f2cf25c44ce327d35b023c8e371fe9e15809
Sherina <137211921+sheri-na@users.noreply.github.com>
2024-04-09 02:52:01 +0000

    Update

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const togglemenu = document.querySelector(".bx-menu")
 togglemenu.onclick = function(){
   dropDownMenu.classList.toggle("open")
 }
+window.onscroll = function () {
+    scrollRotate();
+};
+
+function scrollRotate() {
+    let image = document.getElementById("middle-pic");
+    image.style.transform = "rotate(" + window.pageYOffset / 2 + "deg)";
+}
